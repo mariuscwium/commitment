2cf9e37f67180ac9520e5beca0abb633f0ad9f19
Marius Wium <mcwium@gmail.com>
2018-09-25 20:50:08 +1200

    Started styling.

--- App.js
+++ App.js
@@ -1,43 +1,49 @@
-import React from 'react';
+import React from "react";
 
-import { Provider } from 'react-redux';
-import configureStore from './src/configureStore';
+import { Provider } from "react-redux";
+import configureStore from "./src/configureStore";
 
-import { createStackNavigator } from 'react-navigation';
-import Onboarding from './src/screens/Onboarding';
-import EditCommitment from './src/screens/EditCommitment';
-import FindGym from './src/screens/FindGym';
-import ViewCommitment from './src/screens/ViewCommitment';
+import { createStackNavigator } from "react-navigation";
+import Onboarding from "./src/screens/Onboarding";
+import EditCommitment from "./src/screens/EditCommitment";
+import FindGym from "./src/screens/FindGym";
+import ViewCommitment from "./src/screens/ViewCommitment";
 
 const RootStack = createStackNavigator({
-    Home: {
-        screen: Onboarding,
-        navigationOptions: () => ({
-            header: null
-        })
-    },
-    EditCommitment: {
-        screen: EditCommitment
-    },
-    FindGym: {
-        screen: FindGym
-    },
-    ViewCommitment: {
-        screen: ViewCommitment,
-        navigationOptions: () => ({
-            header: null
-        })
-    }
+  Home: {
+    screen: Onboarding,
+    navigationOptions: () => ({
+      header: null
+    })
+  },
+  EditCommitment: {
+    screen: EditCommitment
+  },
+  FindGym: {
+    screen: FindGym
+  },
+  ViewCommitment: {
+    screen: ViewCommitment,
+    navigationOptions: () => ({
+      header: null
+    })
+  }
 });
 
 const store = configureStore();
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <RootStack />
-            </Provider>
-        );
-    }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <RootStack />
+      </Provider>
+    );
+  }
 }
--- src/screens/EditCommitment.js
+++ src/screens/EditCommitment.js
@@ -1,111 +1,128 @@
-import React from 'react';
-import { View, Text, StyleSheetScrollView, Button, TextInput, Picker } from 'react-native';
-import { MapView } from 'expo';
-import { connect } from 'react-redux';
-import { Formik } from 'formik';
-import { update } from '../modules/commitments';
+import React from "react";
+import { View, Picker } from "react-native";
+import { Container, Button, Text } from "native-base";
+import { MapView } from "expo";
+import { connect } from "react-redux";
+import { Formik } from "formik";
+import { update } from "../modules/commitments";
 
 const { Marker } = MapView;
 class EditCommitment extends React.Component {
-    static navigationOptions = {
-        title: 'Edit your commitment'
-    };
+  static navigationOptions = {
+    title: "Edit your commitment"
+  };
 
-    Location = () => {
-        if (this.props.location) {
-            return (
-                <View style={{ flex: 1 }}>
-                    <MapView
-                        style={{ flex: 1 }}
-                        initialRegion={{
-                            latitude: this.props.location.latitude,
-                            longitude: this.props.location.longitude,
-                            latitudeDelta: 0.01,
-                            longitudeDelta: 0.01
-                        }}
-                        liteMode
-                    >
-                        <Marker coordinate={this.props.location} />
-                    </MapView>
-                    <Button title="Change location" onPress={() => this.props.navigation.navigate('FindGym')} />
-                </View>
-            );
-        } else {
-            return <Button title="Find your gym" onPress={() => this.props.navigation.navigate('FindGym')} />;
-        }
-    };
-
-    updateCommitment = values => {
-        Object.keys(values).map(key => {
-            this.props.updateCommitment({
-                field: key,
-                value: values[key]
-            });
-        });
-        this.props.navigation.navigate('ViewCommitment');
-    };
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <View style={{ flex: 1 }}>{this.Location()}</View>
-                <Formik
-                    style={{ flex: 1 }}
-                    initialValues={{
-                        frequency: 1,
-                        penalty: 5
-                    }}
-                    onSubmit={values => this.updateCommitment(values)}
-                >
-                    {props => (
-                        <View style={{ flex: 1 }}>
-                            <Text> How often can you commit to going to the gym ? </Text>
-                            <Picker
-                                selectedValue={props.values.frequency}
-                                onValueChange={props.handleChange('frequency')}
-                            >
-                                <Picker.Item label="Once a week" value="1" />
-                                <Picker.Item label="Twice a week" value="2" />
-                                <Picker.Item label="Three times a week" value="3" />
-                                <Picker.Item label="Four times a week" value="4" />
-                            </Picker>
-                            <Text> How much are you willing to pay if you don 't go that many times?</Text>
-                            <Picker selectedValue={props.values.penalty} onValueChange={props.handleChange('penalty')}>
-                                <Picker.Item label="$2" value="2" />
-                                <Picker.Item label="$5" value="5" />
-                                <Picker.Item label="$10" value="10" />
-                                <Picker.Item label="$20" value="20" />
-                            </Picker>
-                            {this.props.location && <Button onPress={props.handleSubmit} title="Submit" />}
-                        </View>
-                    )}
-                </Formik>
-            </View>
-        );
+  Location = () => {
+    if (this.props.location) {
+      return (
+        <View style={{ flex: 1 }}>
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude: this.props.location.latitude,
+              longitude: this.props.location.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01
+            }}
+            liteMode
+          >
+            <Marker coordinate={this.props.location} />
+          </MapView>
+          <Button onPress={() => this.props.navigation.navigate("FindGym")}>
+            <Text>Select gym location</Text>
+          </Button>
+        </View>
+      );
+    } else {
+      return (
+        <Button onPress={() => this.props.navigation.navigate("FindGym")}>
+          <Text>Select gym location</Text>
+        </Button>
+      );
     }
+  };
+
+  updateCommitment = values => {
+    Object.keys(values).map(key => {
+      this.props.updateCommitment({
+        field: key,
+        value: values[key]
+      });
+    });
+    this.props.navigation.navigate("ViewCommitment");
+  };
+  render() {
+    return (
+      <Container style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>{this.Location()}</View>
+        <Formik
+          style={{ flex: 1 }}
+          initialValues={{
+            frequency: 1,
+            penalty: 5
+          }}
+          onSubmit={values => this.updateCommitment(values)}
+        >
+          {props => (
+            <View style={{ flex: 1 }}>
+              <Text>How often can you commit to going to the gym ?</Text>
+              <Picker
+                selectedValue={props.values.frequency}
+                onValueChange={props.handleChange("frequency")}
+              >
+                <Picker.Item label="Once a week" value="1" />
+                <Picker.Item label="Twice a week" value="2" />
+                <Picker.Item label="Three times a week" value="3" />
+                <Picker.Item label="Four times a week" value="4" />
+              </Picker>
+              <Text>
+                How much are you willing to pay if you don 't go that many
+                times?
+              </Text>
+              <Picker
+                selectedValue={props.values.penalty}
+                onValueChange={props.handleChange("penalty")}
+              >
+                <Picker.Item label="$2" value="2" />
+                <Picker.Item label="$5" value="5" />
+                <Picker.Item label="$10" value="10" />
+                <Picker.Item label="$20" value="20" />
+              </Picker>
+              {this.props.location && (
+                <Button onPress={props.handleSubmit}>
+                  <Text>Save commitment</Text>
+                </Button>
+              )}
+            </View>
+          )}
+        </Formik>
+      </Container>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    var { location, frequency, penalty } = state.commitments;
-    return {
-        location,
-        frequency,
-        penalty
-    };
+  var { location, frequency, penalty } = state.commitments;
+  return {
+    location,
+    frequency,
+    penalty
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        updateCommitment: ({ field, value }) =>
-            dispatch(
-                update({
-                    field,
-                    value
-                })
-            )
-    };
+  return {
+    updateCommitment: ({ field, value }) =>
+      dispatch(
+        update({
+          field,
+          value
+        })
+      )
+  };
 }
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(EditCommitment);
